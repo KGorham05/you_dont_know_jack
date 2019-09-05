8f436284cbc5838e97771288cc65a9467ec43051
Kgor <kevin.gorham@gmail.com>
2019-09-04 22:18:37 -0700

    created 1 draft of psuedo code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,64 @@
 //PSUEDO CODE TRIVIA GAME
 
-// 
+// Design start game screens/progression 
+// Choose 1 or 2 players 
+// press ENTER to start game
+
+// Display Question One Screen
+    // make this a function that runs and increments a variable to track each time
+// Players (alternating) Chose 1 of 3 categories randomly selected from an array 
+// Display a question from that category 
+// Check to see that hasn't been guessed
+// Display 4 answers
+// Display Cash value of correct guess 
+// Set question timer, display countdown
+
+// If there are 2 players - listen for "q" or "p" to be pressed
+
+// If "q" is pressed, set currentPlayer to playerOne
+// Let player 1 move up with "w" and down with "s", confirm choice with "space-bar"
+
+// If "p" is pressed, set currentPlayer to playerTwo
+// Let player 2 move up with "up-arrow" and down with "down-arrow", confirm choice with "enter"
+
+// If the time runs out - display the correct choice
+
+// Pause timer 
+// If the answer is correct
+    // Update player score variable
+    // Update score displayed on the screen
+    // Move to next question screen
+// If the answer is incorrect 
+    // Check for a 2nd player
+        // If there are 2 players playing
+            // Remove incorrect choice from the DOM
+            // Continue timer 
+            // Listen for select button
+            // If correct/incorrect - show it, update score variable, update score on the DOM
+    // If 1 player game - show correct answer
+        // Deduct score variable + update DOM  
+        // Check for game over condition - use question counter to trigger end of game
+
+// If the game continues... 
+    // Display next question screen -> 
+
+// If the game is over
+    // if there are 2 players
+        // display final scores 
+        // display who won!
+        // generate restart game button
+    // if there is 1 player
+        // show final score
+        // generate restart game button
+        
+
+// ***Stretch Goals***
+
+    // Play audio clip of Question being read aloud - have timer start at audio clip end
+    // Play sound effects for correct/incorrect picks
+    // Create High Score list
+        // Check for high score
+        // Capture player name if gotAHighScore === true; 
+        // Update high score array
+        // Remove Lowest Score on High Score Array
+        // Loop through high score array and display each score on the DOM list
